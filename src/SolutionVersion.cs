1a3c7d4e1ee0cd75a2724a07c83a652d00dc0e7c
Chris Patterson <chris@phatboyg.com>
2012-05-02 10:21:38 -0500

    Built for speed

---
+++ src/SolutionVersion.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("Benchmarque is a benchmarking library and runner")]
+[assembly: AssemblyProduct("Benchmarque")]
+[assembly: AssemblyCopyright("Copyright 2012 Chris Patterson, All rights reserved.")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyInformationalVersion("0.1.0.059c6a")]
+
